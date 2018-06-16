a9363f286b6da6151c3df0a6ba9ee22aa90892c7
Robson Vinicius <robsondgn@outlook.com>
2018-06-16 11:15:58 -0300

    for & variavel.classList.add(classeASerInserida);

--- js/principal.js
+++ js/principal.js
@@ -1,22 +1,40 @@
 var mudarTexto = document.querySelector(".mudar");
 mudarTexto.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for(var i = 0; i < pacientes.length; i++) {
+  var paciente = pacientes[i];
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
 
-var tdGordura = paciente.querySelector(".info-gordura");
-var gordura = tdGordura.textContent;
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
 
+  var tdImc = paciente.querySelector(".info-imc");
 
-var tdImc = paciente.querySelector(".info-imc");
-var imc = peso / (altura * altura);
+  var alturaEhValida = true;
+  var pesoEhValido = true;
 
-tdImc.textContent = imc;
+  if(altura <= 0 || altura >= 3 || peso <= 0 || peso >= 1000)  {
+      alturaEhValido = false;
+      pesoEhValido = false;
+      paciente.classList.add("paciente-invalido");
+  }
 
+  if(alturaEhValida && pesoEhValido) {
+      var imc = peso / (altura * altura);
+      tdImc.textContent = imc.toFixed(2);
+  }else{
+      tdImc.textContent = "Altura e/ou peso inválidos!"
+  }
+}
 
-
+// var cor = document.querySelector(".info-nome");
+// cor.style.color = "rgb(105,150,255)";
+// cor.style.fontSize = "30px";
+// cor.style.fontStyle = "italic";
+// cor.style.fontWeight = "800"
+// var tdGordura = pacientes.querySelector(".info-gordura");
+// var gordura = tdGordura.textContent;
