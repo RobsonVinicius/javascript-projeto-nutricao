8f0cc60b4685ee0e4ea1d5ff94db4596a1aecd97
Robson Vinicius <robsondgn@outlook.com>
2018-11-06 15:51:46 -0200

    Animando a remoção do paciente

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,7 +1,12 @@
 var tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick",function(event){
-  event.target.parentNode.remove();
+  event.target.parentNode.classList.add("fadeOut");
+
+  setTimeout(function(){
+    event.target.parentNode.remove();
+  }, 500)
+  // event.target.parentNode.remove();
 });
 
 // tabela.addEventListener("dblclick",function(event){
